6702e2ace5029b14ee5f24412a689e439d7b1519
hacksang <985438046@qq.com>
2023-12-07 15:26:11 +0800

    remove build script in framework

--- build_example.sh
+++ build_example.sh
@@ -1,5 +1,5 @@
 echo Compiling $1_aarch64...
-aarch64-linux-android32-clang++ examples/$1.cpp utils/Utils.cpp utils/GraphUtils.cpp utils/CommonGraphOptions.cpp -I. -Iinclude -std=c++14 -Wl,--whole-archive -Lbuild/  -larm_compute_graph-static -Wl,--no-whole-archive -larm_compute-static -larm_compute_core-static -L. -o out/$1_aarch64 -static-libstdc++ -pie -DARM_COMPUTE_CL
+aarch64-linux-android32-clang++ examples/$1.cpp utils/Utils.cpp utils/GraphUtils.cpp utils/CommonGraphOptions.cpp -I. -Iinclude -std=c++14 -Wl,--whole-archive -Lbuild/  -larm_compute_graph-static -Wl,--no-whole-archive -larm_compute-static -larm_compute_core-static -L. -o ../../out/$1_aarch64 -static-libstdc++ -pie -DARM_COMPUTE_CL
 echo Done.
 
 
--- build_pa.sh
+++
@@ -1,3 +0,0 @@
-echo "Compile perf_assist..."
-aarch64-linux-android30-clang++ perf_assist.cpp -std=c++14 -o out/perf_assist -static-libstdc++
-echo "Done."
